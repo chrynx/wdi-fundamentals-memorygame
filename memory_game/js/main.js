14549d434be1d2f818178f8be066aa1c8b87744f
CHRYNX <chrynx@CHRYNXs-MacBook-Pro.local>
2017-07-08 16:16:06 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		card: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamons",
+		card: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		card: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		card: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMath = function () {
@@ -14,9 +35,13 @@ var checkForMath = function () {
 }
 var flipCard = function (cardId) {
 
-	console.log('User flipped ' + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log('User flipped ' + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMath();
+	console.log(cards[cardId].card);
+	console.log(cards[cardId].suit);
 }
 flipCard(0);
 flipCard(2);
+
+
